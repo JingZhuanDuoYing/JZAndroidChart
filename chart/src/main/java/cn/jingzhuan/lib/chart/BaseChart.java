9800d42af8f1195a340b49c39c51fba2449f1fd7
donglua <foolfalcon@hotmail.com>
2017-08-23 10:28:30 +0800

    setOnRenderHighlightListener

--- chart/src/main/java/cn/jingzhuan/lib/chart/BaseChart.java
+++ chart/src/main/java/cn/jingzhuan/lib/chart/BaseChart.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 import cn.jingzhuan.lib.chart.component.Highlight;
 import cn.jingzhuan.lib.chart.event.HighlightStatusChangeListener;
-import cn.jingzhuan.lib.chart.event.OnHighlightListener;
+import cn.jingzhuan.lib.chart.event.OnHighlightRenderListener;
 import cn.jingzhuan.lib.chart.renderer.AbstractDataRenderer;
 import cn.jingzhuan.lib.chart.renderer.AxisRenderer;
 import cn.jingzhuan.lib.chart.renderer.Renderer;
@@ -28,7 +28,7 @@ public class BaseChart extends Chart {
 
     protected Highlight[] mHighlights;
     private HighlightStatusChangeListener mHighlightStatusChangeListener;
-    private OnHighlightListener mHighlightListener;
+    private OnHighlightRenderListener mHighlightListener;
 
     public BaseChart(Context context) {
         super(context);
@@ -90,9 +90,6 @@ public class BaseChart extends Chart {
             mHighlightStatusChangeListener.onHighlightShow(highlights);
         }
 
-        if (mHighlightListener != null) {
-            mHighlightListener.highlight(highlights);
-        }
 
         mHighlights = highlights;
 
@@ -124,6 +121,9 @@ public class BaseChart extends Chart {
     public void renderHighlighted(Canvas canvas) {
         if (mRenderer != null) {
             mRenderer.renderHighlighted(canvas, getHighlights());
+            if (getHighlights() != null && mHighlightListener != null) {
+                mHighlightListener.highlight(getHighlights());
+            }
         }
     }
 
@@ -144,7 +144,7 @@ public class BaseChart extends Chart {
         return mHighlightStatusChangeListener;
     }
 
-    public void setOnHighlightListener(OnHighlightListener mHighlightListener) {
+    public void setOnRenderHighlightListener(OnHighlightRenderListener mHighlightListener) {
         this.mHighlightListener = mHighlightListener;
     }
 }
--- chart/src/main/java/cn/jingzhuan/lib/chart/event/OnHighlightRenderListener.java
+++ chart/src/main/java/cn/jingzhuan/lib/chart/event/OnHighlightRenderListener.java
@@ -6,7 +6,7 @@ import cn.jingzhuan.lib.chart.component.Highlight;
  * Created by donglua on 8/23/17.
  */
 
-public interface OnHighlightListener {
+public interface OnHighlightRenderListener {
 
     void highlight(Highlight[] highlights);
 
