e62a90a9ef0994ff1ebea957db7bbeafd6ca81d5
donglua <foolfalcon@hotmail.com>
2017-10-21 14:12:05 +0800

    fix ci build

--- chart/src/androidTest/java/cn/jingzhuan/chart/ExampleInstrumentedTest.java
+++ chart/src/androidTest/java/cn/jingzhuan/chart/ExampleInstrumentedTest.java
@@ -19,6 +19,6 @@ import static org.junit.Assert.*;
     // Context of the app under test.
     Context appContext = InstrumentationRegistry.getTargetContext();
 
-    assertEquals("cn.jingzhuan.lib.test", appContext.getPackageName());
+    assertEquals("cn.jingzhuan.lib.chart.test", appContext.getPackageName());
   }
 }
