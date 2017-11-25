69bb0ee4a7279ad81bae03344e4e8474289c0407
donglua <foolfalcon@hotmail.com>
2017-11-25 10:20:16 +0800

    FunctionalInterface

--- chart/src/main/java/cn/jingzhuan/lib/chart/event/OnHighlightListener.java
+++ chart/src/main/java/cn/jingzhuan/lib/chart/event/OnHighlightListener.java
@@ -5,7 +5,7 @@ import cn.jingzhuan.lib.chart.component.Highlight;
 /**
  * Created by donglua on 8/23/17.
  */
-
+@FunctionalInterface
 public interface OnHighlightListener {
 
     void highlight(Highlight[] highlights);
--- chart/src/main/java/cn/jingzhuan/lib/chart/event/OnViewportChangeListener.java
+++ chart/src/main/java/cn/jingzhuan/lib/chart/event/OnViewportChangeListener.java
@@ -5,7 +5,7 @@ import cn.jingzhuan.lib.chart.Viewport;
 /**
  * Created by Donglua on 17/7/24.
  */
-
+@FunctionalInterface
 public interface OnViewportChangeListener {
 
     void onViewportChange(Viewport viewport);
