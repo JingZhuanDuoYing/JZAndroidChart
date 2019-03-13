d04ac97425b713836200b8f4b16612f389d5c331
donglua <foolfalcon@hotmail.com>
2019-03-13 14:12:15 +0800

    demo v2

--- demo-chart2/src/main/java/cn/jingzhuan/lib/chart2/demo/App.java
+++ demo-chart2/src/main/java/cn/jingzhuan/lib/chart2/demo/App.java
@@ -1,13 +1,13 @@
 package cn.jingzhuan.lib.chart2.demo;
 
-import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 /**
  * Application
  * Created by donglua on 12/27/17.
  */
 
-public class App extends Application {
+public class App extends MultiDexApplication {
 
 
 }
