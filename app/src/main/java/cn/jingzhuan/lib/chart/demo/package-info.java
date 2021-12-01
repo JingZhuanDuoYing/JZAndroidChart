6e1fe496806703aea3cc1e4ac3e3985b4a0f1ed7
JZTech-liangzhenjiang <zhenj1ng@163.com>
2021-12-01 14:26:36 +0800

    fix bug

--- app/src/main/java/cn/jingzhuan/lib/chart/demo/package-info.java
+++ app/src/main/java/cn/jingzhuan/lib/chart/demo/package-info.java
@@ -1,11 +1,13 @@
-@EpoxyDataBindingLayouts({
-    R.layout.layout_desc_text,
-})
-@PackageModelViewConfig(
-    rClass = R.class
-)
 
 package cn.jingzhuan.lib.chart.demo;
 
 import com.airbnb.epoxy.EpoxyDataBindingLayouts;
 import com.airbnb.epoxy.PackageModelViewConfig;
+@EpoxyDataBindingLayouts({
+        R.layout.layout_desc_text,
+})
+@PackageModelViewConfig(
+        rClass = R.class
+)
+interface EpoxyDataBindingConfig {
+}
