303852821b4084481315f07d572e10ad5ab033e1
guobosheng <guobosheng@gmail.com>
2022-02-22 18:29:44 +0800

    规范DataFormatter的单位

--- chart/src/main/java/cn/jingzhuan/lib/chart/data/DataFormatter.java
+++ chart/src/main/java/cn/jingzhuan/lib/chart/data/DataFormatter.java
@@ -4,6 +4,11 @@ import java.util.Locale;
 
 public class DataFormatter {
     public static final String HIDE = "HIDE";
+    public static final String UNIT_TEN_THOUSAND = "万";
+    public static final String UNIT_BILLION = "亿";
+    public static final String UNIT_PERCENT = "%"; // 数值先*100，再添加%为单位
+    public static final String UNIT_CALC = "*"; // 服务器不指定单位，由客户端根据数值范围决定单位。这时精度默认为2
+    public static final String UNIT_EMPTY = "";
     private int precision = 2; // 数据精度
     private String unit = ""; // 数据单位
     private boolean isHide = false;
@@ -15,8 +20,9 @@ public class DataFormatter {
     }
 
     public DataFormatter(int precision, String unit) {
-        if (unit == null || !unit.equals("万") || !unit.equals("亿")
-                || !unit.equals("%") || !unit.equals("")) {
+        if (unit == null || !unit.equals(UNIT_TEN_THOUSAND) || !unit.equals(UNIT_BILLION)
+                || !unit.equals(UNIT_PERCENT)
+                || !unit.equals(UNIT_CALC) || !unit.equals(UNIT_EMPTY)) {
             unit = "";
         }
         if (precision < 0) {
@@ -29,20 +35,20 @@ public class DataFormatter {
     public DataFormatter(int unit) {
         switch (unit) {
             case 1:
-                this.unit = "万";
+                this.unit = UNIT_TEN_THOUSAND;
                 break;
             case 2:
-                this.unit = "亿";
+                this.unit = UNIT_BILLION;
                 break;
             case 3:
-                this.unit = "%";
+                this.unit = UNIT_PERCENT;
                 break;
             case 4:
-                this.unit = "*"; // 服务器不指定单位，由客户端根据数值范围决定单位。这时精度默认为2
+                this.unit = UNIT_CALC; // 服务器不指定单位，由客户端根据数值范围决定单位。这时精度默认为2
                 this.precision = 2;
                 break;
             default:
-                this.unit = "";
+                this.unit = UNIT_EMPTY;
                 this.precision = 2;
                 break;
         }
@@ -66,11 +72,11 @@ public class DataFormatter {
             String[] formatterList = formatterString.toUpperCase(Locale.ROOT).split("_");
             for (String formatter : formatterList) {
                 if (formatter.contains("W")) {
-                    unit = "万";
+                    unit = UNIT_TEN_THOUSAND;
                     String precisionString = formatter.replace("W", "");
                     precision = parsePrecision(precisionString);
                 } else if (formatter.contains("Y")) {
-                    unit = "亿";
+                    unit = UNIT_BILLION;
                     String precisionString = formatter.replace("Y", "");
                     precision = parsePrecision(precisionString);
                 } else if (formatter.contains("F")) {
@@ -78,7 +84,7 @@ public class DataFormatter {
                     String precisionString = formatter.replace("F", "");
                     precision = parsePrecision(precisionString);
                 } else if (formatter.contains("PERCENT")) {
-                    unit = "%";
+                    unit = UNIT_PERCENT;
                 }
             }
 
