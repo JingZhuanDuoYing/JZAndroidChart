9b4fc32f3f724706e6901a208334737618899c29
guobosheng <guobosheng@gmail.com>
2022-11-30 11:41:22 +0800

    v1.8.39; PointValue 增加 mColor 属性，支持指标值正红负绿

--- chart/src/main/java/cn/jingzhuan/lib/chart/data/PointValue.java
+++ chart/src/main/java/cn/jingzhuan/lib/chart/data/PointValue.java
@@ -11,9 +11,15 @@ public class PointValue extends Value {
     private int pathColor = -1; //多段path 不同颜色 应用于: 指标 神龙趋势线
     private float secondValue; //第二条数据 应用于: 指标 高抛低吸
     private float secondY;//第二条数据: y轴坐标
+    private int mColor = -2;
 
     public PointValue(float value) {
         this.value = value;
+    }
+
+    public PointValue(float value, int color) {
+        this.value = value;
+        this.mColor = color;
     }
 
     public PointValue(float value, float secondValue) {
@@ -30,6 +36,14 @@ public class PointValue extends Value {
 
     public void setValue(float value) {
         this.value = value;
+    }
+
+    public int getColor() {
+        return mColor;
+    }
+
+    public void setColor(int color) {
+        mColor = color;
     }
 
     public boolean isPathEnd() {
